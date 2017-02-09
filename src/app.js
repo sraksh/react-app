296ffc4280de7878015dea1f36f05158dd8a8dad
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2017-02-09 07:02:16 +0530

    react app

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import {Greeting} from "./greeting";
+   
+class App extends React.Component {
+  render () {
+   
+    return (
+          <div>
+            <h2>React App </h2>
+            <Greeting />
+ 
+          </div>
+    );
+  }
+}
+
+render(<App/>, document.getElementById('root'));
---
+++ src/greeting.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export class Greeting extends React.Component {
+  render () {
+    return <p> Hello React! from ES6, Babel, Webpack</p>;
+  }
+}
